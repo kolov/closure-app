0044a5565aabef85a70a5a48e941e84edf0adae3
Assen Kolov <assen.kolov@gmail.com>
2012-02-26 20:15:22 +0100

    Heroku

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [compojure "1.0.1"]
                  [clj-stacktrace "0.2.4"]
-                 [ring/ring-core "1.0.1"]
+                 [ring/ring-core "1.0.2"]
                  [clj-http "0.3.0"]
-                 [ring/ring-jetty-adapter "1.0.1"]]
-  :dev-dependencies [[ring/ring-devel "1.0.1"]
+                 [ring/ring-jetty-adapter "1.0.2"]]
+  :dev-dependencies [[ring/ring-devel "1.0.2"]
                      [lein-ring "0.5.4"]
                      [ring-serve "0.1.1"]]
   :main net.kolov.jaclo.server
