d69a0ac7fbdfa4367c542e1f7333d2a45a6ad5e2
Assen Kolov <assen.kolov@gmail.com>
2012-02-26 15:56:04 +0100

    Works with closure

--- resources/public/search.js
+++ resources/public/search.js
@@ -20,16 +20,15 @@ goog.require("goog.ui.AutoComplete.RichRemote");
  *     instances.
  */
 tutorial.notepad.makeNotes = function(data, noteContainer) {
-  var notes = [];
-  for (var i = 0; i < data.length; i++) {
-    var note = 
-      new tutorial.notepad.Note(data[i].title, data[i].content, noteContainer);
-    notes.push(note);
-    note.makeNoteDom();
-  }
-  return notes;
+    var notes = [];
+    for (var i = 0; i < data.length; i++) {
+        var note =
+            new tutorial.notepad.Note(data[i].title, data[i].content, noteContainer);
+        notes.push(note);
+        note.makeNoteDom();
+    }
+    return notes;
 };
-
 
 
 /**
@@ -40,33 +39,35 @@ tutorial.notepad.makeNotes = function(data, noteContainer) {
  *     the notes should be added.
  * @constructor
  */
-tutorial.notepad.Note = function(title, content, noteContainer) {
-  this.title = title;
-  this.content = content;
-  this.parent = noteContainer;
+tutorial.notepad.JavaClass = function(packageName, className, container) {
+    this.packageName = packageName;
+    this.className = className;
+    this.parent = container;
+};
+
+tutorial.notepad.JavaLib = function(packageId, artifactId, container) {
+    this.packageId = packageId;
+    this.artifactId = artifactId;
+    this.parent = container;
 };
 
 
 /**
  * Creates the DOM structure for the note and adds it to the document.
  */
-tutorial.notepad.Note.prototype.makeNoteDom = function() {
-  // Create DOM structure to represent the note.
-  this.headerElement = goog.dom.createDom('div',
-      {'style': 'background-color:#EEE'}, this.title);
-  this.contentElement = goog.dom.createDom('div', null, this.content);
-  var newNote = goog.dom.createDom('div', null,
-      this.headerElement, this.contentElement);
-
-  // Add the note's DOM structure to the document.
-  goog.dom.appendChild(this.parent, newNote);
-  return new goog.ui.Zippy(this.headerElement, this.contentElement);
+tutorial.notepad.JavaClass.prototype.makeDom = function() {
+    goog.dom.appendChild(this.parent, goog.dom.createDom('div', {'class': 'clazz'}, this.packageName + "." + this.className));
 };
 
-tutorial.notepad.makeRichRow_ = function(item, itemType, itemClassName) {
-      item.type = itemType;
+tutorial.notepad.JavaLib.prototype.makeDom = function() {
+    goog.dom.appendChild(this.parent, goog.dom.createDom('div', {'class': 'clazz'}, this.packageId + ":" + this.artifactId));
+};
 
-      item.render = function(node, token) {
+
+tutorial.notepad.makeRichRow_ = function(item, itemType, itemClassName) {
+    item.type = itemType;
+
+    item.render = function(node, token) {
         var dom_ = goog.dom.getDomHelper(node);
         var typeNode = dom_.createDom("span", itemClassName);
         dom_.appendChild(typeNode, dom_.createTextNode(itemType));
@@ -76,28 +77,35 @@ tutorial.notepad.makeRichRow_ = function(item, itemType, itemClassName) {
 
         dom_.appendChild(node, typeNode);
         dom_.appendChild(node, nameNode);
-      };
-
-      item.select = function(target) {
-        target.value = item.name;
-      };
-
-      return item;
     };
+
+    item.select = function(target) {
+        target.value = item.name;
+    };
+
+    return item;
+};
 
 tutorial.notepad.apple = function(item) {
-      return tutorial.notepad.makeRichRow_(item, "Apple", "apple");
-    };
+    return tutorial.notepad.makeRichRow_(item, "Apple", "apple");
+};
 
 tutorial.notepad.citrus = function(item) {
-      return tutorial.notepad.makeRichRow_(item, "Citrus", "citrus");
-    };
+    return tutorial.notepad.makeRichRow_(item, "Citrus", "citrus");
+};
 
 tutorial.notepad.berry = function(item) {
-      return tutorial.notepad.makeRichRow_(item, "Berry", "berry");
-    };
+    return tutorial.notepad.makeRichRow_(item, "Berry", "berry");
+};
+
+javaClass = function(item) {
+    return tutorial.notepad.makeRichRow_(item, "Class", "class");
+};
 
 
+totalJavaClasses = function(item) {
+    return tutorial.notepad.makeRichRow_(item, "Class", "class");
+};
 
     
     
