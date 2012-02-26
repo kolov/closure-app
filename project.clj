4cb7cf6d7fa42f7c7795e1e02e4c2edef79a5a3c
Assen Kolov <assen.kolov@gmail.com>
2012-02-26 17:39:06 +0100

    Heroku fix

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject cloure-app "1.0.0-SNAPSHOT"
-  :description "hello-clojure-web: an example app for my blog."
+  :description "ClojureScript/Google Composure app"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [compojure "1.0.1"]
                  [clj-stacktrace "0.2.4"]
@@ -9,4 +9,5 @@
   :dev-dependencies [[ring/ring-devel "0.3.7"]
                      [lein-ring "0.5.4"]
                      [ring-serve "0.1.1"]]
+  :main 'net.kolov.jaclo.core
   :ring {:handler net.kolov.jaclo.core/app})
