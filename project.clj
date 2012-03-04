2846d0d5cc1d92796a48013257902480c9787274
Assen Kolov <assen.kolov@gmail.com>
2012-03-05 00:59:11 +0100

    All clojurescript

--- project.clj
+++ project.clj
@@ -5,9 +5,17 @@
                  [clj-stacktrace "0.2.4"]
                  [ring/ring-core "1.1.0-SNAPSHOT"]
                  [clj-http "0.3.0"]
+                 [jayq "0.1.0-SNAPSHOT"]
                  [ring/ring-jetty-adapter "1.1.0-SNAPSHOT"]]
   :dev-dependencies [[ring/ring-devel "1.1.0-SNAPSHOT"]
                      [lein-ring "0.5.4"]
                      [ring-serve "0.1.1"]]
   :main net.kolov.jaclo.server
-  :ring {:handler net.kolov.jaclo.core/app})
+  :ring {:handler net.kolov.jaclo.core/app}
+  :cljsbuild {
+          :source-path "src-cljs"
+          :compiler {
+            :output-to "resources/public/main.js"
+            :optimizations :whitespace
+            :pretty-print true}}
+  )
