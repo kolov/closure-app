0847b1ac7cd2f4659ed9e45a90384dfad4a6cc72
Assen Kolov <assen.kolov@gmail.com>
2012-03-06 18:09:36 +0100

    cljsbuild in project.clj

--- project.clj
+++ project.clj
@@ -9,7 +9,8 @@
                  [ring/ring-jetty-adapter "1.1.0-SNAPSHOT"]]
   :dev-dependencies [[ring/ring-devel "1.1.0-SNAPSHOT"]
                      [lein-ring "0.5.4"]
-                     [ring-serve "0.1.1"]]
+                     [ring-serve "0.1.1"]
+                     [lein-cljsbuild "0.1.2"]]
   :main net.kolov.jaclo.server
   :ring {:handler net.kolov.jaclo.core/app}
   :cljsbuild {
